c7ea9dca87536867fba56b909bd6f0e07dc1e291
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-12-01 15:57:45 -0500

    Header and nav style

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,7 +2,10 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [openOptions, setOpenOptions] = useState(true)
   const { user } = useContext(AuthContext);
 
   return (
@@ -11,7 +14,26 @@ const Navbar = () => {
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">booking-app</span>
         </Link>
-        {user ? <div className="username">{user.username}</div> : (
+        {user ? (
+          <>
+            <div className="username" onClick={() => setOpenOptions(prev => !prev)}>{user.username}</div>
+            {openOptions && ( 
+              <div className="user-options">
+                  <ul>
+                    <li onClick={() => setOpenOptions(prev => !prev)}>
+                      <p>Logout</p>
+                    </li>
+                    <li onClick={() => setOpenOptions(prev => !prev)}>
+                      <p>Settings</p>
+                    </li>
+                    <li onClick={() => setOpenOptions(prev => !prev)}>
+                      <p>Profile</p>
+                    </li>
+                  </ul>
+              </div>
+            )}
+          </>
+         ) : (
           <div className="navItems">
             <button className="navButton">Register</button>
             <Link to="/login" style={{ color: "inherit", textDecoration: "none" }}>
